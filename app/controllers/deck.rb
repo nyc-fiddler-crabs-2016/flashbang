9ed744a9ae8b22cab9a6d4c6f4cc32f3f67f1167
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-05 18:23:18 -0500

    add delete and edite options for decks. begin user auth for these processes- but decks have no user_ids

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -34,3 +34,46 @@ get '/decks/:id' do
   @deck = Deck.find(params[:id])
   erb :'decks/show'
 end
+
+
+get '/decks/:id/edit' do
+  @deck = Deck.find(params[:id])
+  # if logged_in? && current_user.id == @deck.user_id
+    erb :'decks/edit'
+  # # else
+  #   status 403
+  #   redirect
+  # end
+end
+
+
+put '/decks/:id' do
+  @deck = Deck.find(params[:id])
+ # if logged_in? && current_user.id == @deck.user_id
+#   # erb :'decks/show'
+# end
+  @deck.update(name: params[:name])
+  erb :'/cards/new'
+end
+
+
+# delete '/decks/:id' do
+#  deck = Deck.find(params[:id])
+#   # if logged_in? && current_user.id == @deck.user_id
+#     deck.destroy
+#     redirect '/'
+#   # else
+#   #   status 403
+#   #   redirect
+#   # end
+# end
+
+
+
+
+
+
+
+
+
+
