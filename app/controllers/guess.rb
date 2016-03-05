3e50780446b097c1ac9483338aaadb1c59f44f40
Joe Case <jcase146@gmail.com>
2016-03-05 13:23:08 -0500

    Get previous card result logic working.

--- app/controllers/guess.rb
+++ app/controllers/guess.rb
@@ -3,7 +3,10 @@ post '/guesses' do
   status = card.guess_correct?(params[:guess])
   guess = Guess.create(game_id: params[:game_id], card_id: params[:card_id], status: status)
   game = Game.find(params[:game_id])
+  session[:previous_guess] = guess
+
   if game.game_over?
+    session[:previous_guess] = nil
     redirect '/'
   else
     redirect "/games/#{params[:game_id]}"
--- app/models/game.rb
+++ app/models/game.rb
@@ -33,13 +33,7 @@ class Game < ActiveRecord::Base
     self.guesses.where(status: true).count
   end
 
-  def self.game_over?
-    total_correct_guesses == self.cards.count
-  end
 
-  # def play_game
-
-  # end
 
   def correct_first_try
     #Guesses where(status:true) && Guess.card_id
